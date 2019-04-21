58472cc9556c30cd0d1c367a45b0b8b235c3bca6
nasriime <mnasr4040@gmail.com>
2019-04-21 15:30:40 +0200

    get departments count

--- routes/staff.route.js
+++ routes/staff.route.js
@@ -12,6 +12,45 @@ router.get('/', (req, res, next) => {
     });
 });
 
+router.get('/department', (req, res, next) => {
+    const deptName = req.query.deptName;
+    if( deptName ){
+        Staff.aggregate([
+            {
+                $match: { 
+                    department: deptName 
+                }
+            },
+            {
+                $group: { 
+                    _id: "$department",
+                    count: {"$sum": 1}}
+            }
+        ], (err, staff) =>{
+            if (err){
+              res.send(err);
+            } else {
+              res.status(200).json(staff);
+            }
+        });
+    }else{
+        Staff.aggregate([
+            {
+                $group: { 
+                    _id: "$department",
+                    count: {"$sum": 1}}
+            }
+        ], (err, staff) =>{
+            if (err){
+              res.send(err);
+            } else {
+              res.status(200).json(staff);
+            }
+        });
+    }
+    
+});
+
 router.get('/:id', (req, res, next) => {
     Staff.findOne( {id: req.params.id } , (err, staff) => {
         if (err){
